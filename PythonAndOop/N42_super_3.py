62b0535fa4c8f8a4dd3550d5aac0c05e8a7b6e04
Liu Dong <839327350@qq.com>
2020-09-12 23:03:38 +0800

    OOP阶段总结4

---
+++ PythonAndOop/N42_super_3.py
@@ -0,0 +1,23 @@
+class A:
+    def foo(self):
+        print("A")
+
+
+class B(A):
+    # def foo(self):
+    #     print("B")
+    pass
+
+class C(A):
+    def foo(self):
+        print("C")
+        super(C, self).foo()
+
+
+class D(B, C):
+    def foo(self):
+        print("D")
+        super(D, self).foo()
+
+d = D()
+d.foo()
