81cfdb00df01665dec799fcc985e19ef175e8bd4
Liu Dong <839327350@qq.com>
2020-09-15 21:51:57 +0800

    项目培训初步完成

--- project_train/deal_pdf.py
+++ project_train/deal_pdf.py
@@ -1,60 +1,53 @@
+import io
 import os
 import re
+import time
+import zipfile
+
 import requests
 
-class DealPdf2Table:
-    def __init__(self):
-        self.base_dir=os.path.join(os.getcwd(),'company')
-        self.company_names=os.listdir(self.base_dir)
 
-        #table,wbk,html,html-scale,layout,extract-classes
-        self.query = {
-                    'extract-image': 'true',
-                    'format': 4,                #1 or 4 规定是4
-                    'textlines': 'false',       #testlinse or span
-                    'table': 'ybk',             #wbk ybk all
-                    #'wbk': 2,                   #wbk的识别方式2快1慢
-                    #'layout': 'default',
-                    #'html': 'false',
-                    #'html-scale': 1.5,
-                    #'extract-classes': 'chart', #chart表示需要识别图表，diagram表示需要识别流程图等，多个使用逗号分割
-                    'output-files': 'raw.json', #表示同时返回raw.json，多个使用逗号分割
-                    'output-format': 'zip',     #zip json json表示仅仅返回table.json文件，zip表示使用zip打包多个文件，bz2使用tar.bz2打包多个文件返回
+#网站自带的函数封装了一个类,改变了一下保存路径,发现一个bug:zipfile没有open属性
+class Pdf2TableAPI:
+    def invoke_api(self, api, filename, company_name, file_name_without_pdf):
+        # 异步执行
+        output_format = 'zip'  # or json or bz2
+        url = 'http://qianliyan2.memect.cn:6111/api/{}'.format(api)
+        query = {
+            # 表示异步执行，所以需要通过轮训获得结果
+            'async': 'true',
+            'output-format': output_format
         }
+        headers = {}
+        with open(filename, 'rb') as fp:
+            data = fp.read()
+        # 如果上传的是json，建议先压缩，如：
+        # import gzip
+        # data = gzip.compress(data)
+        # headers={'Content-Encoding':'gzip'}
+        r = requests.post(url, data=data, params=query, headers=headers)
+        result = None
+        if r.status_code == 200:
+            result = r.json()
+        elif r.status_code == 400:
+            # 返回的是json
+            result = r.json()
+        else:
+            # 其它的错误，如：500，系统错误
+            print(r.text)
 
-        self.headers = {}
-        self.url = 'http://qianliyan2.memect.cn:6111/api/pdf2table'
-    def deal_pdf_2table_with_1thread(self,company_index):
+        # 等待结果
+        if result and result.get('task'):
+            # result={task:{id:''}}
+            self.get_result(api, result.get('task').get('id'), company_name, file_name_without_pdf,
+                            output_format=output_format)
+        else:
+            # 执行失败等
+            pass
 
-        #company_files
-        company_path=os.path.join(self.base_dir,self.company_names[company_index])
-        files_list=os.listdir(company_path)
-        print(files_list)
-        for file_name in files_list:
-            print(file_name)
-            try:
-                if re.findall('回复',file_name)[0]=='回复':
-                    file_path=os.path.join(self.base_dir,self.company_names[company_index],file_name)
-                    print(file_path)
-
-                    with open(file_path, 'rb') as fp:
-                        data = fp.read()
-                    r = requests.post(self.url, data=data, params=self.query, headers=self.headers)
-                    if r.status_code == 200:
-                        # 返回的是json，或者使用r.content，直接保存到文件中
-                        r.json()
-                    elif r.status_code == 400:
-                        # 返回错误
-                        r.json()
-                    else:
-                        # 其它的错误
-                        pass
-            except:
-                pass
-
-    def get_result(api, task_id, output_format='json'):
+    def get_result(self, api, task_id, company_name, file_name_without_pdf, output_format='json'):
         # api=pdf2json ,pdf2table,pdf2doc
-        url = 'http://127.0.0.1:6111/api/{}'.format(api)
+        url = 'http://qianliyan2.memect.cn:6111/api/{}'.format(api)
         query = {
             'task_id': task_id
         }
@@ -67,13 +60,14 @@ class DealPdf2Table:
                         if output_format == 'bz2':
                             import tarfile
                             with tarfile.open(fileobj=fp) as tar:
-                                tar.extractall('local/a')
+                                tar.extractall('company/{}/{}'.format(company_name, file_name_without_pdf))
                         else:
                             import zipfile
-                            with zipfile.open(fp) as zf:
-                                zf.extractall('local/a')
+                            with zipfile.ZipFile(fp) as zf:  # with zipfile.open(fp) as zf 报错 没有这个属性
+                                zf.extractall('company/{}/{}'.format(company_name, file_name_without_pdf))
+                                print('zip文件下载并解压成功')
                 else:
-                    with open('local/a.json', 'wb') as fp:
+                    with open('company/{}/{}.json'.format(company_name, file_name_without_pdf), 'wb') as fp:
                         fp.write(r.content)
                 break
             elif r.status_code == 400:
@@ -94,5 +88,53 @@ class DealPdf2Table:
                 # 其他的错误，如：500，系统错误，不需要再轮训
                 break
 
+
+class DealPdf2Table(Pdf2TableAPI):
+    def __init__(self):
+        self.base_dir = os.path.join(os.getcwd(), 'company')
+        self.company_names = os.listdir(self.base_dir)
+
+        # table,wbk,html,html-scale,layout,extract-classes
+        self.query = {
+            'extract-image': 'true',
+            'format': 4,  # 1 or 4 规定是4
+            'textlines': 'false',  # testlinse or span
+            'table': 'ybk',  # wbk ybk all
+            # 'wbk': 2,                   #wbk的识别方式2快1慢
+            # 'layout': 'default',
+            # 'html': 'false',
+            # 'html-scale': 1.5,
+            # 'extract-classes': 'chart', #chart表示需要识别图表，diagram表示需要识别流程图等，多个使用逗号分割
+            'output-files': 'raw.json',  # 表示同时返回raw.json，多个使用逗号分割
+            'output-format': 'zip',  # zip json json表示仅仅返回table.json文件，zip表示使用zip打包多个文件，bz2使用tar.bz2打包多个文件返回
+        }
+
+        self.headers = {}
+        self.url = 'http://qianliyan2.memect.cn:6111/api/pdf2table'
+
+    def deal_pdf_2table_with_1thread(self, company_index):
+        # company_files
+        company_path = os.path.join(self.base_dir, self.company_names[company_index])
+        print('公司名字为:{}'.format(self.company_names[company_index]))
+        files_list = os.listdir(company_path)
+        print(files_list)
+        for file_name in files_list:
+            print(file_name)
+            try:
+                if re.findall('回复', file_name)[0] == '回复':
+                    file_path = os.path.join(self.base_dir, self.company_names[company_index], file_name)
+                    print(file_path)
+
+                    print('开始调用invoke_api')
+                    self.invoke_api(api='pdf2table', filename=file_path, company_name=self.company_names[company_index],
+                                    file_name_without_pdf=file_name[:-4])
+                    # api, filename,company_name,file_name_without_pdf
+            except:
+                print('该文件没有回复字样')
+
+
 if __name__ == '__main__':
-    DealPdf2Table().deal_pdf_2table_with_1thread(1)
+    companys_path=os.path.join(os.getcwd(),'company')
+    companys_number=len(os.listdir(companys_path))
+    for company_index in range(companys_number):
+        DealPdf2Table().deal_pdf_2table_with_1thread(company_index)
